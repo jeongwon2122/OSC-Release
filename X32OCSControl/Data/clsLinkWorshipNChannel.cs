574a0dc4203d1a44f1e257cf94b02d6ca553a7cb
jeongwon2122 <qrkblack3@gmail.com>
2025-01-31 13:39:36 +0900

    파일 삭제 반영

--- X32OCSControl/Data/clsLinkWorshipNChannel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace X32OSCControl.Data
-{
-    public class clsLinkWorshipNChannel
-    {
-        public int worship_index { get; set; } 
-        public int channel_index { get; set; }
-    }
-}
